6053666d3ddc833a06ee280788b39db0211b7fec
telavip <megateca@outlook.com>
2025-03-05 18:34:10 -0300

    Add files via upload

---
+++ serie127.js
@@ -0,0 +1,124 @@
+window.series127Data = {
+"202555": {
+    "id": 202555,
+    "name": "Demolidor: Renascido",
+    "slug": "demolidor-renascido",
+    "original_title": "Daredevil: Born Again",
+    "overview": "Matt Murdock, um advogado cego com habilidades fantásticas, luta por justiça através de seu agitado escritório de advocacia, enquanto o ex-chefe do crime Wilson Fisk busca seus próprios empreendimentos políticos em Nova Iorque. Quando suas identidades passadas começam a emergir, seus caminhos se cruzam perigosamente.",
+    "first_air_date": "2025-03-04",
+    "episode_run_time": 0,
+    "vote_average": 8.9,
+    "number_of_seasons": 1,
+    "number_of_episodes": 9,
+    "created_by": [
+      "Dario Scardapane",
+      "Chris Ord",
+      "Matt Corman"
+    ],
+    "production_companies": [
+      "Marvel Studios",
+      "Kevin Feige Productions"
+    ],
+    "genres": [
+      "Drama",
+      "Crime"
+    ],
+    "certification": "18",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/qHCpG49qxs4owLgmhh0T62ErUZv.jpg",
+    "poster_path": "https://image.tmdb.org/t/p/original/eODcpeaj6jFoVG1kBVpNFyFh45I.jpg",
+    "cast": [
+      "Charlie Cox",
+      "Vincent D'Onofrio",
+      "Margarita Levieva",
+      "Deborah Ann Woll",
+      "Elden Henson"
+    ],
+    "trailer": "N/A",
+    "seasons": {
+      "1": {
+        "name": "Temporada 1",
+        "poster_path": "https://image.tmdb.org/t/p/original/eODcpeaj6jFoVG1kBVpNFyFh45I.jpg",
+        "backdrop_path": "N/A",
+        "air_date": "2025-03-04",
+        "episode_count": 9,
+        "overview": "N/A",
+        "vote_average": 9.4,
+        "episodes": {
+          "1": {
+            "name": "Meia Hora do Céu",
+            "overview": "Matt Murdock desiste da máscara. Wilson Fisk almeja novos patamares.",
+            "air_date": "2025-03-04",
+            "runtime": 61,
+            "still_path": "https://image.tmdb.org/t/p/original/x9gnlDmtWyMGzYV6I846an4Fp3r.jpg",
+            "vote_average": 8.667
+          },
+          "2": {
+            "name": "Ótica",
+            "overview": "Matt Murdock ganha um novo cliente que é mais do que diz ser. Fisk aprende o poder da ótica.",
+            "air_date": "2025-03-04",
+            "runtime": 50,
+            "still_path": "https://image.tmdb.org/t/p/original/seP1v27FZ6Ph9313QXkxkig43tA.jpg",
+            "vote_average": 9.8
+          },
+          "3": {
+            "name": "Episódio 3",
+            "overview": "N/A",
+            "air_date": "2025-03-11",
+            "runtime": 0,
+            "still_path": "https://image.tmdb.org/t/p/original/cWUd5tpH7ydQdDnBlZT0zMoepuj.jpg",
+            "vote_average": 10
+          },
+          "4": {
+            "name": "Episódio 4",
+            "overview": "N/A",
+            "air_date": "2025-03-18",
+            "runtime": 0,
+            "still_path": "https://image.tmdb.org/t/p/original/l3Kt2dqStHhcl39iq6qBl2sF2BH.jpg",
+            "vote_average": 0
+          },
+          "5": {
+            "name": "Episódio 5",
+            "overview": "N/A",
+            "air_date": "2025-03-25",
+            "runtime": 0,
+            "still_path": "N/A",
+            "vote_average": 0
+          },
+          "6": {
+            "name": "Episódio 6",
+            "overview": "N/A",
+            "air_date": "2025-03-25",
+            "runtime": 0,
+            "still_path": "https://image.tmdb.org/t/p/original/m4zAmcqMF9RpKNQNYpIYcNErJQ2.jpg",
+            "vote_average": 0
+          },
+          "7": {
+            "name": "Episódio 7",
+            "overview": "N/A",
+            "air_date": "2025-04-01",
+            "runtime": 0,
+            "still_path": "https://image.tmdb.org/t/p/original/5lIryvVphn2iMAuQXWQOW785oDP.jpg",
+            "vote_average": 0
+          },
+          "8": {
+            "name": "Episódio 8",
+            "overview": "N/A",
+            "air_date": "2025-04-08",
+            "runtime": 0,
+            "still_path": "N/A",
+            "vote_average": 0
+          },
+          "9": {
+            "name": "Episódio 9",
+            "overview": "N/A",
+            "air_date": "2025-04-15",
+            "runtime": 0,
+            "still_path": "N/A",
+            "vote_average": 0
+          }
+        }
+      }
+    }
+  },
+
+};
