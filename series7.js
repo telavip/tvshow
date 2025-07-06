164cb8dcab81029c7dbfc6503565ed7547095912
telavip <megateca@outlook.com>
2025-07-05 21:50:26 -0300

    Update series7.js

--- series7.js
+++ series7.js
@@ -19,5 +19,329 @@ const series7 = [
   "certification": "18",
   "episode_count": 9
 },
+{
+  "id": 60625,
+  "slug": "rick-e-morty",
+  "type": "desenho",
+  "title": "Rick e Morty",
+  "original_title": "Rick and Morty",
+  "genre_ids": [
+    16,
+    35,
+    10765,
+    10759
+  ],
+  "first_air_date": "2013-12-02",
+  "popularity": 198.946,
+  "vote_average": "8.69",
+  "poster_path": "https://image.tmdb.org/t/p/w200/5qfd0e2uMbVInX3YdeFbDsfxi1t.jpg",
+  "networks": [
+    80
+  ],
+  "certification": "16",
+  "episode_count": 81
+},
+{
+  "id": 54650,
+  "slug": "power",
+  "type": "serie",
+  "title": "Power",
+  "original_title": "Power",
+  "genre_ids": [
+    80,
+    18
+  ],
+  "first_air_date": "2014-06-07",
+  "popularity": 38.2911,
+  "vote_average": "7.665",
+  "poster_path": "https://image.tmdb.org/t/p/w200/ctftzvyj8b0odco7EoS9VfJhf7K.jpg",
+  "networks": [
+    318
+  ],
+  "certification": "16",
+  "episode_count": 63
+},
+{
+  "id": 97890,
+  "slug": "power-book-ii-ghost",
+  "type": "serie",
+  "title": "Power Book II: Ghost",
+  "original_title": "Power Book II: Ghost",
+  "genre_ids": [
+    80,
+    18
+  ],
+  "first_air_date": "2020-09-06",
+  "popularity": 87.0592,
+  "vote_average": "8.3",
+  "poster_path": "https://image.tmdb.org/t/p/w200/8rbxvwGpPd9MWjdhAoSBj016cns.jpg",
+  "networks": [
+    318
+  ],
+  "certification": "16",
+  "episode_count": 40
+},
+{
+  "id": 124394,
+  "slug": "power-book-iii-raising-kanan",
+  "type": "serie",
+  "title": "Power Book III: Raising Kanan",
+  "original_title": "Power Book III: Raising Kanan",
+  "genre_ids": [
+    18,
+    80
+  ],
+  "first_air_date": "2021-07-18",
+  "popularity": 40.2513,
+  "vote_average": "8.256",
+  "poster_path": "https://image.tmdb.org/t/p/w200/pZ7IaHON9hnu8C0g3zdoWwIsJ9t.jpg",
+  "networks": [
+    318
+  ],
+  "certification": "16",
+  "episode_count": 40
+},
+{
+  "id": 119845,
+  "slug": "power-book-iv-force",
+  "type": "serie",
+  "title": "Power Book IV: Force",
+  "original_title": "Power Book IV: Force",
+  "genre_ids": [
+    80,
+    18
+  ],
+  "first_air_date": "2022-02-06",
+  "popularity": 17.5429,
+  "vote_average": "8.341",
+  "poster_path": "https://image.tmdb.org/t/p/w200/cR0ursLXoT0zfX9XDGAzQRAnK0W.jpg",
+  "networks": [
+    318
+  ],
+  "certification": "16",
+  "episode_count": 20
+},
+{
+  "id": 101591,
+  "slug": "bmf",
+  "type": "serie",
+  "title": "BMF",
+  "original_title": "BMF",
+  "genre_ids": [
+    18,
+    80
+  ],
+  "first_air_date": "2021-09-26",
+  "popularity": 31.5828,
+  "vote_average": "8.034",
+  "poster_path": "https://image.tmdb.org/t/p/w200/5gUlUufL3fom30qxKawwlWuD7kC.jpg",
+  "networks": [
+    318
+  ],
+  "certification": "16",
+  "episode_count": 32
+},
+{
+  "id": 248830,
+  "slug": "stick",
+  "type": "serie",
+  "title": "Stick",
+  "original_title": "Stick",
+  "genre_ids": [
+    35,
+    18
+  ],
+  "first_air_date": "2025-06-03",
+  "popularity": 54.0752,
+  "vote_average": "6.768",
+  "poster_path": "https://image.tmdb.org/t/p/w200/y8EWrf5Ry1WmYksWT7MOPWexvr5.jpg",
+  "networks": [
+    2552
+  ],
+  "certification": "14",
+  "episode_count": 10
+},
+{
+  "id": 114471,
+  "slug": "coracao-de-ferro",
+  "type": "serie",
+  "title": "Coração de Ferro",
+  "original_title": "Ironheart",
+  "genre_ids": [
+    10765,
+    80
+  ],
+  "first_air_date": "2025-06-24",
+  "popularity": 254.2086,
+  "vote_average": "5.693",
+  "poster_path": "https://image.tmdb.org/t/p/w200/yRanvKZhy5NdF64amF0WNOAIuQu.jpg",
+  "networks": [
+    2739
+  ],
+  "certification": "14",
+  "episode_count": 6
+},
+{
+  "id": 81723,
+  "slug": "a-idade-dourada",
+  "type": "serie",
+  "title": "A Idade Dourada",
+  "original_title": "The Gilded Age",
+  "genre_ids": [
+    18
+  ],
+  "first_air_date": "2022-01-24",
+  "popularity": 46.2139,
+  "vote_average": "7.8",
+  "poster_path": "https://image.tmdb.org/t/p/w200/agrNZZDXsOThmNFB0cjXeb35BvQ.jpg",
+  "networks": [
+    49
+  ],
+  "certification": "16",
+  "episode_count": 25
+},
+{
+  "id": 260458,
+  "slug": "adultos",
+  "type": "serie",
+  "title": "Adultos",
+  "original_title": "Adults",
+  "genre_ids": [
+    35
+  ],
+  "first_air_date": "2025-05-28",
+  "popularity": 22.623,
+  "vote_average": "5.708",
+  "poster_path": "https://image.tmdb.org/t/p/w200/z5c1KTvUd8CeRlkBd8c3Kp3yUEV.jpg",
+  "networks": [
+    88
+  ],
+  "certification": "18",
+  "episode_count": 8
+},
+{
+  "id": 215522,
+  "slug": "mentirosos",
+  "type": "serie",
+  "title": "Mentirosos",
+  "original_title": "We Were Liars",
+  "genre_ids": [
+    18,
+    9648
+  ],
+  "first_air_date": "2025-06-18",
+  "popularity": 75.1173,
+  "vote_average": "6.772",
+  "poster_path": "https://image.tmdb.org/t/p/w200/cLwxFcO4pyji3YwkKBnM42a0yQf.jpg",
+  "networks": [
+    1024
+  ],
+  "certification": "16",
+  "episode_count": 8
+},
+{
+  "id": 279060,
+  "slug": "chespirito-sem-querer-querendo",
+  "type": "serie",
+  "title": "Chespirito: Sem Querer Querendo",
+  "original_title": "Chespirito: Sin querer queriendo",
+  "genre_ids": [
+    18,
+    35
+  ],
+  "first_air_date": "2025-06-05",
+  "popularity": 401.2487,
+  "vote_average": "8.031",
+  "poster_path": "https://image.tmdb.org/t/p/w200/nNicd3JYAjbbo93WCpgBc6UJzrF.jpg",
+  "networks": [
+    6783
+  ],
+  "certification": "12",
+  "episode_count": 8
+},
+{
+  "id": 241554,
+  "slug": "diarios-de-um-robo-assassino",
+  "type": "serie",
+  "title": "Diários de um Robô-Assassino",
+  "original_title": "Murderbot",
+  "genre_ids": [
+    35,
+    18,
+    10765
+  ],
+  "first_air_date": "2025-05-15",
+  "popularity": 98.8214,
+  "vote_average": "7.462",
+  "poster_path": "https://image.tmdb.org/t/p/w200/rWQYtLHVzG44Ks6Ohi6K1IVyIwq.jpg",
+  "networks": [
+    2552
+  ],
+  "certification": "16",
+  "episode_count": 10
+},
+{
+  "id": 232230,
+  "slug": "lord-of-mysteries",
+  "type": "serie",
+  "title": "Lord of Mysteries",
+  "original_title": "诡秘之主",
+  "genre_ids": [
+    16,
+    9648,
+    10765
+  ],
+  "first_air_date": "2025-06-28",
+  "popularity": 140.7378,
+  "vote_average": "9.6",
+  "poster_path": "https://image.tmdb.org/t/p/w200/kBRyCCZWipMO5Gu9Y02nE3iMgr0.jpg",
+  "networks": [
+    2007
+  ],
+  "certification": "N/A",
+  "episode_count": 13
+},
+{
+  "id": 254013,
+  "slug": "revival",
+  "type": "serie",
+  "title": "Revival",
+  "original_title": "Revival",
+  "genre_ids": [
+    10765,
+    80,
+    9648
+  ],
+  "first_air_date": "2025-06-12",
+  "popularity": 20.7099,
+  "vote_average": "6.5",
+  "poster_path": "https://image.tmdb.org/t/p/w200/5CuMd9uyNlN9BAxnE29B19pFenZ.jpg",
+  "networks": [
+    77,
+    5337
+  ],
+  "certification": "N/A",
+  "episode_count": 10
+},
+{
+  "id": 113959,
+  "slug": "duster",
+  "type": "serie",
+  "title": "Duster",
+  "original_title": "Duster",
+  "genre_ids": [
+    80,
+    18
+  ],
+  "first_air_date": "2025-05-15",
+  "popularity": 50.1388,
+  "vote_average": "6",
+  "poster_path": "https://image.tmdb.org/t/p/w200/h3cbwxUdUev04z2KBEsvvJ2QwYE.jpg",
+  "networks": [
+    6783
+  ],
+  "certification": "16",
+  "episode_count": 8
+},
 
 ];
